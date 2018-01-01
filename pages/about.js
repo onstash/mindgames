53380654cb5cb9c4f20098e5d3c1ac82d60e0e52
Santosh Venkatraman <santosh.venk@gmail.com>
2018-01-01 22:47:59 +0530

    Adds simple header component

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
+import Header from '../components/header';
+
 export default () => (
   <div>
+    <Header />
     <p>This is the about page</p>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 
+import Header from '../components/header';
+
 const Index = () => (
   <div>
-    <Link href="/about">
-      <a>About Page</a>
-    </Link>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 );
